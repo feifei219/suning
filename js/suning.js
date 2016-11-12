185aa90dbe58bc319c1e66fda0295fd593ac3b4d
feifei219 <1175807702@qq.com>
2016-11-13 00:50:27 +0800

    初始化

---
+++ js/suning.js
@@ -0,0 +1,18 @@
+/**
+ * Created by feifei on 2016/10/28.
+ */
+var mySwiper = new Swiper ('.banner .swiper-container', {
+        loop: true,
+        autopaly:2000,
+        pagination: '.banner .swiper-pagination'
+    })
+var mySwiper = new Swiper ('.lunbo1 .swiper-container', {
+    loop: true,
+    autopaly:2000,
+    pagination: '.lunbo1 .swiper-pagination'
+})
+var mySwiper = new Swiper ('.lunbo2 .swiper-container', {
+    loop: true,
+    autopaly:2000,
+    pagination: '.lunbo2 .swiper-pagination'
+})
